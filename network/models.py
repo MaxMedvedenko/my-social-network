99a0e704ad812af57c85f49692353b2222e85de1
MaxMedvedenko <medvedenkomaksim143@gmail.com>
2024-06-14 18:52:24 +0300

    changes

--- network/models.py
+++ network/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.content[:20]
+        return self.content[:40]
 
 # Модель лайків
 class Like(models.Model):
--- user/models.py
+++ user/models.py
@@ -13,7 +13,6 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     bio = models.TextField(max_length=500, blank=True)
     is_admin = models.BooleanField(default=False)
-
     def __str__(self):
         return self.username
 
