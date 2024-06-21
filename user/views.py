f67815b43384104dba8ad1846993867d93a0db59
MaxMedvedenko <medvedenkomaksim143@gmail.com>
2024-06-21 17:38:53 +0300

    small changes and changes in login and register view

--- user/views.py
+++ user/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 
 from django.contrib.auth import login as auth_login, login, logout, authenticate
 
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
@@ -25,7 +24,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return redirect('index')  # URL of the main page
+            return redirect('index')
         else:
             error_message = "Неправильне ім'я користувача або пароль. Будь ласка, спробуйте ще раз."
 
@@ -43,17 +42,21 @@ def register_view(request):
         last_name = request.POST.get('last_name')
 
         if password1 == password2:
-            try:
-                user = User(username=username, email=email, first_name=first_name, last_name=last_name)
-                user.set_password(password1)  # Hash the password
-                user.full_clean()  # Trigger validation
-                user.save()
-                login(request, user)  # Log the user in after registration
-                return redirect('index')  # Redirect to the main page URL
-            except IntegrityError:
+            if User.objects.filter(username=username).exists():
                 error_message = "Ім'я користувача вже існує. Будь ласка, виберіть інше ім'я користувача."
-            except ValidationError as e:
-                error_message = e.message
+            else:
+                try:
+                    user = User(username=username, email=email, first_name=first_name, last_name=last_name)
+                    user.set_password(password1)
+                    user.full_clean()
+                    user.save()
+                    login(request, user)
+                    return redirect('index')
+                except ValidationError as e:
+                    error_message = e.messages[0]
+                except IntegrityError as e:
+                    error_message = "Ім'я користувача вже існує. Будь ласка, виберіть інше ім'я користувача."
+                    print(f"IntegrityError: {e}")
         else:
             error_message = "Паролі не співпадають."
 
