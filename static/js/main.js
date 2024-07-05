494afceb087d509314352ec72c17edc9a5e53cb0
MaxMedvedenko <medvedenkomaksim143@gmail.com>
2024-07-05 14:46:58 +0300

    changes in js for popup menu

--- static/js/main.js
+++ static/js/main.js
@@ -1,42 +1,19 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Select all elements with the class 'dots-vertical-svg'
-//     const toggleButtons = document.querySelectorAll('.dots-vertical-svg');
-  
-//     // Add event listeners to each toggle button
-//     toggleButtons.forEach(function(toggleButton) {
-//       toggleButton.addEventListener('click', function(e) {
-//         e.stopPropagation(); // Stop the event from bubbling up to the document
-  
-//         // Find the corresponding dropdown-content element
-//         const popupMenu = toggleButton.nextElementSibling;
-  
-//         // Toggle the display of the popup menu
-//         if (popupMenu.style.display === 'none' || popupMenu.style.display === '') {
-//           // Close any open popup menus
-//           document.querySelectorAll('.dropdown-content').forEach(function(menu) {
-//             menu.style.display = 'none';
-//           });
-//           // Open the current popup menu
-//           popupMenu.style.display = 'block';
-//         } else {
-//           // Close the current popup menu
-//           popupMenu.style.display = 'none';
-//         }
-//       });
-//     });
-//   });
-  
+//--- for popup menu ---//
+
 document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('dots-vertical-svg');
-    const dropdownContent = toggleButton.nextElementSibling;
+    const toggleButtons = document.querySelectorAll('.dots-vertical-svg');
 
-    toggleButton.addEventListener('click', function() {
-        dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
-    });
+    toggleButtons.forEach(toggleButton => {
+        const dropdownContent = toggleButton.nextElementSibling;
 
-    document.addEventListener('click', function(e) {
-        if (!dropdownContent.contains(e.target) && e.target !== toggleButton) {
-            dropdownContent.style.display = 'none';
-        }
+        toggleButton.addEventListener('click', function() {
+            dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+        });
+
+        document.addEventListener('click', function(e) {
+            if (!dropdownContent.contains(e.target) && e.target !== toggleButton) {
+                dropdownContent.style.display = 'none';
+            }
+        });
     });
 });
